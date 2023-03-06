0fb67f4a0d9c9e3ca85211e34af8dbddd914fdfd
kagr <kevin@goldsteinconsultingservices.com>
2023-03-05 23:25:38 -0500

    setting default cores

--- src/main/java/com/gcs/tools/latency/recorders/LatencyRecorderProperties.java
+++ src/main/java/com/gcs/tools/latency/recorders/LatencyRecorderProperties.java
@@ -41,122 +41,122 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public final class LatencyRecorderProperties implements IProps
 {
-	public static final String	PROCESSOR_CORE_ID	= "ThreadControl.ProcessingCoreId";
-	public static final String	WRITER_CORE_ID		= "ThreadControl.WriterCoreId";
+    public static final String PROCESSOR_CORE_ID = "ThreadControl.ProcessingCoreId";
+    public static final String WRITER_CORE_ID    = "ThreadControl.WriterCoreId";
 
-	public static final String	CAPACITY		= "Procesor.Capacity";
-	public static final String	SINGLE_WRITER	= "Type.Recorder.SingleWriter";
-	public static final String	SIMPLE_RECORDER	= "Type.SimpleRecorder";
+    public static final String CAPACITY        = "Procesor.Capacity";
+    public static final String SINGLE_WRITER   = "Type.Recorder.SingleWriter";
+    public static final String SIMPLE_RECORDER = "Type.SimpleRecorder";
 
-	public static final String	BUFFER_COUNT	= "Writer.BufferCount";
-	public static final String	BUFFER_RATIO	= "Writer.BufferRatio";
-	public static final String	OUTFILE_NAME	= "Writer.OutfileName";
-	public static final String	OUTFILE_PATH	= "Writer.OutfilePath";
-
-
-
-
-	@NonNull private String _name;
-
-	@NonNull private int _expectedMsgRage;
-
-
-	private String	_fname;
-	private String	_fpath;
-
-	private int	_processorCoreId;
-	private int	_writerCoreId;
-
-
-
-	/**
-	 * number of entries available to the ring buffer
-	 */
-	private int _capacity;
-
-	/**
-	 * number of integers to make available in a write bugger (the total size of
-	 * the buffer is this value * 4)
-	 */
-	private int	_ratioByteBufferEntries;
-	private int	_cntBuffers;
-
-
-
-	/**
-	 * lmax-disruptor wait strategy, should be derived
-	 */
-	private WaitStrategy _waitStrategy;
-
-
-	/**
-	 * indicates to use the most basic of recording types (no threads, simple in
-	 * memory array)
-	 */
-	private boolean _simpleRecorder = false;
-
-
-	/**
-	 * if the _simpleRecorder=fasle, then use a sinlgeWriter for the full
-	 * version? in other words, do I expect more than one concurrent writer
-	 */
-	private ProducerType _producerType;
+    public static final String BUFFER_COUNT = "Writer.BufferCount";
+    public static final String BUFFER_RATIO = "Writer.BufferRatio";
+    public static final String OUTFILE_NAME = "Writer.OutfileName";
+    public static final String OUTFILE_PATH = "Writer.OutfilePath";
 
 
 
 
-	public String getKeyFqn(@NonNull final String key_)
-	{
-		return String.format("LatencyRecorder.%s.%s", _name, key_);
-	}
+    @NonNull private String _name;
+
+    @NonNull private int _expectedMsgRage;
+
+
+    private String _fname;
+    private String _fpath;
+
+    private int _processorCoreId = -1;
+    private int _writerCoreId    = -1;
+
+
+
+    /**
+     * number of entries available to the ring buffer
+     */
+    private int _capacity;
+
+    /**
+     * number of integers to make available in a write bugger (the total size of
+     * the buffer is this value * 4)
+     */
+    private int _ratioByteBufferEntries;
+    private int _cntBuffers;
+
+
+
+    /**
+     * lmax-disruptor wait strategy, should be derived
+     */
+    private WaitStrategy _waitStrategy;
+
+
+    /**
+     * indicates to use the most basic of recording types (no threads, simple in
+     * memory array)
+     */
+    private boolean _simpleRecorder = false;
+
+
+    /**
+     * if the _simpleRecorder=fasle, then use a sinlgeWriter for the full
+     * version? in other words, do I expect more than one concurrent writer
+     */
+    private ProducerType _producerType;
 
 
 
 
-
-	public Properties getAsProps()
-	{
-		Properties props = new Properties();
-		props.setProperty(getKeyFqn(CAPACITY), Integer.toString(_capacity));
-		props.setProperty(getKeyFqn(BUFFER_COUNT), Integer.toString(_cntBuffers));
-		props.setProperty(getKeyFqn(BUFFER_RATIO), Integer.toString(_ratioByteBufferEntries));
-		props.setProperty(getKeyFqn(OUTFILE_NAME), _fname);
-		props.setProperty(getKeyFqn(OUTFILE_PATH), _fpath);
-		props.setProperty(getKeyFqn(SIMPLE_RECORDER), Boolean.toString(_simpleRecorder));
-		props.setProperty(getKeyFqn(PROCESSOR_CORE_ID), Integer.toString(_processorCoreId));
-		props.setProperty(getKeyFqn(WRITER_CORE_ID), Integer.toString(_writerCoreId));
-		return props;
-	}
+    public String getKeyFqn(@NonNull final String key_)
+    {
+        return String.format("LatencyRecorder.%s.%s", _name, key_);
+    }
 
 
 
 
 
-	@Override
-	public void loadFromXml(@NonNull XMLConfiguration cfg_) throws ConfigurationException
-	{
-
-	}
-
-
-
-
-
-	@Override
-	public Map<String, String> toMap()
-	{
-		return null;
-	}
+    public Properties getAsProps()
+    {
+        Properties props = new Properties();
+        props.setProperty(getKeyFqn(CAPACITY), Integer.toString(_capacity));
+        props.setProperty(getKeyFqn(BUFFER_COUNT), Integer.toString(_cntBuffers));
+        props.setProperty(getKeyFqn(BUFFER_RATIO), Integer.toString(_ratioByteBufferEntries));
+        props.setProperty(getKeyFqn(OUTFILE_NAME), _fname);
+        props.setProperty(getKeyFqn(OUTFILE_PATH), _fpath);
+        props.setProperty(getKeyFqn(SIMPLE_RECORDER), Boolean.toString(_simpleRecorder));
+        props.setProperty(getKeyFqn(PROCESSOR_CORE_ID), Integer.toString(_processorCoreId));
+        props.setProperty(getKeyFqn(WRITER_CORE_ID), Integer.toString(_writerCoreId));
+        return props;
+    }
 
 
 
 
 
-	@Override
-	public String toString()
-	{
-		return getAsProps().toString();
-	}
+    @Override
+    public void loadFromXml(@NonNull XMLConfiguration cfg_) throws ConfigurationException
+    {
+
+    }
+
+
+
+
+
+    @Override
+    public Map<String, String> toMap()
+    {
+        return null;
+    }
+
+
+
+
+
+    @Override
+    public String toString()
+    {
+        return getAsProps().toString();
+    }
 
 
 
